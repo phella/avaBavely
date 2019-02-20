5ff4e6c30228ce7505cb2ef45458c6859b5f8a4c
phella <philopateernabil2021@gmail.com>
2019-02-20 18:45:35 +0200

    adding loading spinner for routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { slideInAnimation } from './app.animation';
-
+import {Event, Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,4 +9,17 @@ import { slideInAnimation } from './app.animation';
 })
 export class AppComponent {
   title = 'avaBavely';
+  loading = true;
+  constructor(private router: Router) {
+    router.events.subscribe((routerEvet: Event) => {
+      this.checkRouterEvent(routerEvet);
+    });
+  }
+    checkRouterEvent(routerEvent: Event): void {
+      if ( routerEvent instanceof NavigationStart) {
+        this.loading = true;
+      } else {
+        this.loading = false;
+      }
+    }
 }
